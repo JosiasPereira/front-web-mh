df0797275e89c00f2b278244fba33fdcc2df1b42
Josias <josiassilva32@gmail.com>
2020-02-18 16:30:42 -0300

    change url server

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,2 +1,2 @@
-//export const URL_SERVER = "https://williamestrela.herokuapp.com";
-export const URL_SERVER = "http://localhost:3333";
+export const URL_SERVER = "https://williamestrela.herokuapp.com";
+//export const URL_SERVER = "http://localhost:3333";
